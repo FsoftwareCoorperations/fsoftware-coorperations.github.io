f043f8cdb11c57b215a4846f52e928dec93f8a78
StarLand <smirkogm@gmail.com>
2015-06-11 17:50:54 +0200

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -2,4 +2,12 @@
 function website() { 
   var homepage = window.alert("http://fsoftwarecoorperations.github.io/fsoftware-coorperations.github.io/");
 }
+function error() { 
+  var ans = confirm("Anything was wrong.\nRetry?");
+  if (ans == true) {
+    window.alert("Hello World");
+  } else {
+    window.alert("Hello World");
+  }
+}
 </script>
